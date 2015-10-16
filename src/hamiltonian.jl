092af200e38f69f5583edb8b70d0332f654d3f88
Stefano Battaglia <stefano.battaglia@irsamc.ups-tlse.fr>
2015-10-16 17:58:08 +0200

    attempt to first structure of Hamiltonian

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,2 +1,13 @@
-# Define abstract type for Hamiltonians
-abstract Hamiltonian
+# Define parametric abstract type for Hamiltonians
+# T defines the type of the matrix
+type Hamiltonian{T}
+    matrix::Array{T,2}        # Hamiltonian matrix always of dimension 2
+    nrwos::Int64              # Int64 according to output of size() function
+    ncols::Int64              # Uint64 would probably be better?
+end
+
+type TightBinding{Real} <: Hamiltonian{Real}
+    eta::Float64
+    sites::Int64
+    pbc::bool
+end
