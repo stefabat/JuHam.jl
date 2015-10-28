ef39b0567bf724248879ebd4619aa9eb0cf19641
Stefano Battaglia <stefano.battaglia@irsamc.ups-tlse.fr>
2015-10-28 16:28:32 +0100

    compute_tps divided in serial and parallel. parallel wrong result

--- src/measurement.jl
+++ src/measurement.jl
@@ -7,6 +7,26 @@ end
 
 # Function to compute the TPS in any dimension with an arbitrary operator
 function compute_tps(hamiltonian::Hamiltonian, topology::Topology, operator::Function)
+    xyz = operator(topology.xyz)
+    eig_vectors = hamiltonian.eig_vectors
+    N = hamiltonian.nrows
+    n_electrons = convert(Int64,N/2)
+    tps = zeros(1,3)
+    ndim = topology.ndim
+    for i = 1:n_electrons
+        for j = n_electrons+1:N
+            eigensquared = eig_vectors[:,i].*eig_vectors[:,j]
+            for k = 1:ndim
+                tps[k] += 2.0*dot(eigensquared,xyz[:,k])^2
+            end
+        end
+    end
+    return tps
+end
+
+# Parallel version: NOT WORKING
+# Function to compute the TPS in any dimension with an arbitrary operator
+function compute_tps_parallel(hamiltonian::Hamiltonian, topology::Topology, operator::Function)
     xyz = convert(SharedArray,operator(topology.xyz))
     eig_vectors = convert(SharedArray,hamiltonian.eig_vectors)
     N = hamiltonian.nrows
@@ -15,7 +35,7 @@ function compute_tps(hamiltonian::Hamiltonian, topology::Topology, operator::Fun
     ndim = topology.ndim
     for coord = 1:ndim
         for i = 1:n_electrons
-            tps[coord] = @sync @parallel (+) for j = n_electrons+1:N
+            tps[coord] = @parallel (+) for j = n_electrons+1:N
                 2.0*dot((eig_vectors[:,i].*eig_vectors[:,j]),xyz[:,coord])^2
             end
         end
