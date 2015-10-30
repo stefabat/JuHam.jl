8a187c03fc12372639010d9dfaab5ff3750610a1
Stefano Battaglia <stefano.battaglia@irsamc.ups-tlse.fr>
2015-10-30 17:58:04 +0100

    format function to save output

--- src/JuHam.jl
+++ src/JuHam.jl
@@ -4,8 +4,10 @@ module JuHam
     include("measurement.jl")
     include("input.jl")
     include("simulation.jl")
+    include("output.jl")
 
     export polyene_model_inpgen, polyene_real_inpgen, polyene_circle_inpgen, generate_multiple_inputs
     export polyene_tps_simulation
     export Input
+    export write_out
 end
