6f09c166ebd34e11bb8e01816627332f67e9d76b
Stefano Battaglia <battaglia1803@gmail.com>
2021-07-04 17:02:59 +0200

    update tps function

--- src/properties.jl
+++ src/properties.jl
@@ -1,27 +1,29 @@
-# Measurements function
 
 # Total position spread tensor and polarizability
 # of a given wavefunction and topology
 # NOTE: only valid in the Huckel approximation
 # NOTE: polarizability currently restricted to xx, yy and zz components
 # TODO: compute also xy, xz and yz polarizabilities
-function compute_tps(wave_function::WaveFunction, topology::Topology, MOenergies)
-    N      = wave_function.basis.dim
-    M      = round(Int64,N/2)
+# function tps(wave_function::WaveFunction, topology::Topology, MOenergies)
+function tps(H::Hamiltonian, energies::AbstractVector{Float64}, states::AbstractMatrix{Float64})
+
+    M = size(energies,1)
+    N = round(Int64,N/2)
+
     ndim   = topology.dim
     coords = zeros(N,ndim)
 	eigvec = wave_function.MOcoeffs
     tps    = zeros(1,ndim)
-    pola   = zeros(1,ndim)
+    # pola   = zeros(1,ndim)
 
     # TODO: to parallelize
     for i = 1:M         # Loop over occupied orbitals
         for j = M+1:N   # Loop over empty orbitals
-            eigsq = eigvec[:,i].*eigvec[:,j]
+            eigsq = states[:,i].*states[:,j]
             for k = 1:ndim
                 dotprod  = dot(eigsq,coords[:,k])^2
                 tps[k]  += 2.0*dotprod
-                pola[k] += 4.0*dotprod/(MOenergies[j] - MOenergies[i])
+                # pola[k] += 4.0*dotprod/(MOenergies[j] - MOenergies[i])
             end
         end
     end
